10125158b811a6e6c6013a9fe6ca34a513dd67f8
Pascal Post <pascal-post@mailbox.org>
2026-01-12 19:12:01 +0100

    change theme and add theme toggle

--- src/lib/components/site-header.svelte
+++ src/lib/components/site-header.svelte
@@ -1,1 +1,27 @@
-<header class="sticky top-0 z-50 flex w-full items-center border-b">TurboMesh</header>
+<script lang="ts">
+	import SidebarIcon from '@lucide/svelte/icons/sidebar';
+	import SunIcon from '@lucide/svelte/icons/sun';
+	import MoonIcon from '@lucide/svelte/icons/moon';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import { toggleMode } from 'mode-watcher';
+</script>
+
+<header class="sticky top-0 z-50 flex w-full items-center border-b">
+	<div class="flex h-(--header-height) w-full items-center gap-2 px-4">
+		<Button class="size-8" variant="ghost" size="icon"><SidebarIcon /></Button>
+		<Separator orientation="vertical" class="me-2 h-4" />
+		TurboMesh
+		<div class="ml-auto">
+			<Button onclick={toggleMode} variant="outline" size="icon">
+				<SunIcon
+					class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
+				/>
+				<MoonIcon
+					class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
+				/>
+				<span class="sr-only">Toggle theme</span>
+			</Button>
+		</div>
+	</div>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,21 @@
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import MonacoEditor from '$lib/components/MonacoEditor.svelte';
 	import SiteHeader from '$lib/components/site-header.svelte';
-	import { Play } from '@lucide/svelte/icons';
+	import Play from '@lucide/svelte/icons/play';
 </script>
 
-<Sidebar.Provider class="flex flex-col">
-	<SiteHeader />
-	<Resizable.PaneGroup direction="horizontal" class="h-full">
-		<Resizable.Pane class="flex flex-col">
-			<Sidebar.Root class="static! w-full!">
-				<MonacoEditor language="json" />
-				<Button><Play />Generate Grid</Button>
-			</Sidebar.Root>
-		</Resizable.Pane>
-		<Resizable.Handle />
-		<Resizable.Pane>Two</Resizable.Pane>
-	</Resizable.PaneGroup>
-</Sidebar.Provider>
+<div class="[--header-height:calc(--spacing(14))]">
+	<Sidebar.Provider class="flex flex-col">
+		<SiteHeader />
+		<Resizable.PaneGroup direction="horizontal" class="h-full">
+			<Resizable.Pane class="flex flex-col">
+				<Sidebar.Root class="static! w-full!">
+					<MonacoEditor language="json" />
+					<Button><Play />Generate Grid</Button>
+				</Sidebar.Root>
+			</Resizable.Pane>
+			<Resizable.Handle />
+			<Resizable.Pane>Two</Resizable.Pane>
+		</Resizable.PaneGroup>
+	</Sidebar.Provider>
+</div>
