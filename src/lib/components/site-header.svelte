005479435c0bc1965200deee312257e00542389c
Pascal Post <pascal-post@mailbox.org>
2026-01-12 17:52:37 +0100

    add header and remove test page

---
+++ src/lib/components/site-header.svelte
@@ -0,0 +1,1 @@
+<header class="sticky top-0 z-50 flex w-full items-center border-b">TurboMesh</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
 	import * as Resizable from '$lib/components/ui/resizable/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import MonacoEditor from '$lib/components/MonacoEditor.svelte';
+	import SiteHeader from '$lib/components/site-header.svelte';
+	import { Play } from '@lucide/svelte/icons';
 </script>
 
-<div class="h-dvh">
-	<Resizable.PaneGroup direction="horizontal" class="min-h-50 max-w-md rounded-lg border">
-		<Resizable.Pane defaultSize={25}>
-			<Sidebar.Provider>
-				<Sidebar.Root class="static!" />
-			</Sidebar.Provider>
+<Sidebar.Provider class="flex flex-col">
+	<SiteHeader />
+	<Resizable.PaneGroup direction="horizontal" class="h-full">
+		<Resizable.Pane class="flex flex-col">
+			<Sidebar.Root class="static! w-full!">
+				<MonacoEditor language="json" />
+				<Button><Play />Generate Grid</Button>
+			</Sidebar.Root>
 		</Resizable.Pane>
-		<Resizable.Handle withHandle />
-		<Resizable.Pane defaultSize={75}>Two</Resizable.Pane>
+		<Resizable.Handle />
+		<Resizable.Pane>Two</Resizable.Pane>
 	</Resizable.PaneGroup>
-</div>
+</Sidebar.Provider>
