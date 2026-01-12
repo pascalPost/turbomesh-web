861aade6d14bc4bb3f383d3b92c47d98b02fc485
Pascal Post <pascal-post@mailbox.org>
2026-01-12 17:20:00 +0100

    add shadcn-svelte based layout

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Resizable from '$lib/components/ui/resizable/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import MonacoEditor from '$lib/components/MonacoEditor.svelte';
+</script>
+
+<Sidebar.Provider>
+	<Resizable.PaneGroup direction="horizontal" class="h-full">
+		<Resizable.Pane class="flex flex-col">
+			<Sidebar.Root class="static! w-full!">
+				<MonacoEditor language="json" />
+				<Button>Generate Grid</Button>
+			</Sidebar.Root>
+		</Resizable.Pane>
+		<Resizable.Handle />
+		<Resizable.Pane>Two</Resizable.Pane>
+	</Resizable.PaneGroup>
+</Sidebar.Provider>
